3dc54e1787eba1499a91d8f45caf90144ecaaac8
siavash <siavash.wdev@gmail.com>
2024-10-01 10:52:38 +0330

    feat: implement getAllProducts and saveProduct to save new and edited products

--- src/js/storage.js
+++ src/js/storage.js
@@ -14,14 +14,33 @@ export default class Storage {
       existedCategory.description = categoryToSave.description;
     } else {
       // save new category
-      const category = {
-        id: new Date().getTime(),
-        title: categoryToSave.title,
-        description: categoryToSave.description,
-        createdAt: new Date().toISOString(),
-      };
-      allCategories.push(category);
+      categoryToSave.id = new Date().getTime();
+      categoryToSave.createdAt = new Date().toISOString();
+      allCategories.push(categoryToSave);
     }
     localStorage.setItem('categories', JSON.stringify(allCategories));
+  }
+
+  static getAllProducts() {
+    const savedProducts = JSON.parse(localStorage.getItem('products')) || [];
+    savedProducts.sort((a, b) => (a > b) ? -1 : 1);
+    return savedProducts;
+  }
+
+  static saveProduct(productToSave) {
+    const allProducts = this.getAllProducts();
+    const existedProduct = allProducts.find((product) => product.id === productToSave.id);
+    if (existedProduct) {
+      // save edited product
+      existedProduct.title = productToSave.title;
+      existedProduct.quantity = productToSave.quantity;
+      existedProduct.category = productToSave.category;
+    } else {
+      // save new product
+      productToSave.id = new Date().getTime();
+      productToSave.createdAt = new Date().toISOString();
+      allProducts.push(productToSave);
+    }
+    localStorage.setItem('products', JSON.stringify(allProducts));
   }
 }
