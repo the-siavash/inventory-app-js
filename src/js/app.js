504a206ef1555f01eba412ac9656f1786d078c2e
siavash <siavash.wdev@gmail.com>
2024-10-12 02:35:21 +0330

    feat: add light and dark mode toggler

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log(CategoryView.categories);
   console.log(ProductView.products);
 });
+
+// dark mode toggler
+const toggler = document.querySelector('#toggler');
+const lightMode = document.querySelector('#light-mode');
+const darkMode = document.querySelector('#dark-mode');
+toggler.addEventListener('click', (event) => {
+  if (event.target.id === 'light-mode') {
+    lightMode.classList.add('hidden');
+    darkMode.classList.remove('hidden');
+    document.querySelector('html').classList.add('dark');
+  } else {
+    lightMode.classList.remove('hidden');
+    darkMode.classList.add('hidden');
+    document.querySelector('html').classList.remove('dark');
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     },
     extend: {},
   },
+  darkMode: 'selector',
   plugins: [
     require('@tailwindcss/forms'),
   ],
